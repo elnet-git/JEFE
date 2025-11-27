c91c5fba23e0d275996879c9181b46d646d6881f
TuNombreDeGitHub <tuemail@github.com>
2025-11-27 15:11:59 -0600

    Actualización de jefe: arreglos y mejoras

--- abrir_jefe.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-import os
-
-# Ruta absoluta de tu HTML
-ruta_html = r"C:\Users\infob\Desktop\jefe\jefe.html"
-
-# Abrir en navegador por defecto
-webbrowser.open(f"file:///{ruta_html}")
